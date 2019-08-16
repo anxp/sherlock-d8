f5c8e8013dfeefd24aab7e1bd97e896d6e525239
Andrey Kaplunenko <me@kaplunenko.name>
2019-08-16 22:56:40 +0300

    Refactored MarketFetchController - core functionality moved to special method to increase reusability.

--- web/modules/sherlock_d8/src/Controller/MarketFetchController.php
+++ web/modules/sherlock_d8/src/Controller/MarketFetchController.php
@@ -13,36 +13,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Drupal\sherlock_d8\CoreClasses\ItemSniper\{ItemSniper, olx_ItemSniper, bsp_ItemSniper, skl_ItemSniper};
 use Drupal\sherlock_d8\CoreClasses\ArrayFiltration_2D\ArrayFiltration_2D;
 use Drupal\sherlock_d8\CoreClasses\FileManager\FileManager;
-use Twig\Error\RuntimeError;
 
 class MarketFetchController extends ControllerBase {
-  public function fetchMarkets() {
-    if (!isset($_POST['market_id'])) {
-      $response = new JsonResponse();
-      $response->setData([]); //TODO: Make this error return more informative, maybe...
-      return $response;
-    }
-
+  public function fetchMarketCore(string $marketID, array $urlsSetForGivenMarket, int $priceFrom = null, int $priceTo = null): array {
     $itemSniperFullNamespacePath = '\Drupal\sherlock_d8\CoreClasses\ItemSniper\\';
-
-    //Get marketID to fetch. This ID is passed from frontend by Ajax/POST request. So we check $_POST for it:
-    $marketID = $_POST['market_id'];
-    unset($_POST['market_id']);
-
-    //Next step - get collection of search URL queries for given $marketID,
-    //which are stored in $_SESSION['sherlock_tmp_storage']['constructed_urls_collection'][$marketID]:
-    $searchURLs_by_marketID = $_SESSION['sherlock_tmp_storage']['constructed_urls_collection'][$marketID];
 
     //Construct Object Name.
     // As a result we will have Fully Qualified Object Name like "\Drupal\sherlock_d8\CoreClasses\ItemSniper\olx_ItemSniper":
     $objName = $itemSniperFullNamespacePath.$marketID . '_ItemSniper';
 
     $snipeRawResults = [];
-    for ($i = 0; $i < count($searchURLs_by_marketID); $i++) {
-      $sniperObject = new $objName($searchURLs_by_marketID[$i], 5); //Create new object of somemarket_ItemSniper.
+    for ($i = 0; $i < count($urlsSetForGivenMarket); $i++) {
+      $sniperObject = new $objName($urlsSetForGivenMarket[$i], 5); //Create new object of somemarket_ItemSniper.
       $oneQueryResult = $sniperObject->grabItems();
       unset($sniperObject);
-      $snipeRawResults[$searchURLs_by_marketID[$i]] = $oneQueryResult;
+      $snipeRawResults[$urlsSetForGivenMarket[$i]] = $oneQueryResult;
     }
 
     //TODO - Here is perfect place to cache search results ($snipeRawResults)
@@ -62,15 +47,9 @@ class MarketFetchController extends ControllerBase {
     $filteredData = ArrayFiltration_2D::selectUniqueSubArrays_byField($rawResultsFlattened, 'link');
 
     //If user specified price range - let's do such filtration!
-    $price_from = $_SESSION['sherlock_tmp_storage']['price_from'];
-    $price_to = $_SESSION['sherlock_tmp_storage']['price_to'];
-    $filteredData = ArrayFiltration_2D::selectSubArrays_byFieldValueRange($filteredData, 'price_value', $price_from, $price_to);
+    $filteredData = ArrayFiltration_2D::selectSubArrays_byFieldValueRange($filteredData, 'price_value', $priceFrom, $priceTo);
 
     //At the very last step - cache images (or use already cached if they are exists):
-    /**
-     * TODO: Implement Caching.
-     */
-
     foreach ($filteredData as &$value) {
       $pic = new FileManager('public://sherlock/img_cache');
       $picExtUrl = $value['thumbnail'];
@@ -79,6 +58,29 @@ class MarketFetchController extends ControllerBase {
       unset($pic);
     }
     unset($value);
+
+    return $filteredData;
+  }
+
+  public function fetchGivenMarket() {
+    if (!isset($_POST['market_id'])) {
+      $response = new JsonResponse();
+      $response->setData([]); //TODO: Make this error return more informative, maybe...
+      return $response;
+    }
+
+    //Get marketID to fetch. This ID is passed from frontend by Ajax/POST request. So we check $_POST for it:
+    $marketID = $_POST['market_id'];
+    unset($_POST['market_id']);
+
+    //Next step - get collection of search URL queries for given $marketID,
+    //which are stored in $_SESSION['sherlock_tmp_storage']['constructed_urls_collection'][$marketID]:
+    $searchURLs_by_marketID = $_SESSION['sherlock_tmp_storage']['constructed_urls_collection'][$marketID];
+
+    $priceFrom = $_SESSION['sherlock_tmp_storage']['price_from'];
+    $priceTo = $_SESSION['sherlock_tmp_storage']['price_to'];
+
+    $filteredData = $this->fetchMarketCore($marketID, $searchURLs_by_marketID, $priceFrom, $priceTo);
 
     $response = new JsonResponse();
     $response->setData($filteredData);
